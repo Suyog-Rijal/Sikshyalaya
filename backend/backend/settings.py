9e5a2e4f4956820da5b79cd0731943b0ce1dc1cf
Suyog-Rijal <rijalsuyog75@gmail.com>
2025-04-28 17:58:37 +0545

    General bug fixex frontend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -99,13 +99,24 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# postgres
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Sikshyalaya',
+        'USER': 'postgres',
+        'PASSWORD': 'Suyog.07',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
